9cfa298d743433357c15d9fa5f5d681fbc979cc9
monster <772148702@qq.com>
2017-12-23 14:07:40 +0800

    添加了部分移动平台UI,由于模型复杂动画逻辑，跳跃按钮暂时没有添加成功

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+    Vector3 offset;
+    // Use this for initializationC
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v1 = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, v1, smoothing * Time.deltaTime);
+    }
+}
